852b93b44391c120097837cccb62d23c90f11f4a
vinayAkkana <vinay_akkana@harvard.edu>
2014-08-14 14:48:55 -0400

    Reverting back portion of the last change related to adding full path to classpath as Windows cannot handle long values.

--- fits.bat
+++ fits.bat
@@ -10,12 +10,15 @@ set FITS_HOME=%PRGDIR:~0,-1%
 set JARS=
 set CLASSPATH=
 
-for %%i in (%FITS_HOME%\lib\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
-for %%i in (%FITS_HOME%\lib\droid\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
-for %%i in (%FITS_HOME%\lib\jhove\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
-for %%i in (%FITS_HOME%\lib\nzmetool\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
-for %%i in (%FITS_HOME%\lib\nzmetool\adapters\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
+cd %FITS_HOME%
+
+for %%i in (lib\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
+for %%i in (lib\droid\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
+for %%i in (lib\jhove\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
+for %%i in (lib\nzmetool\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
+for %%i in (lib\nzmetool\adapters\*.jar) do call "%FITS_HOME%\cpappend.bat" %%i
 
 set CLASSPATH=%JARS%;%FITS_HOME%\xml\nlnz
+rem echo %CLASSPATH%
 
 java edu.harvard.hul.ois.fits.Fits %*
