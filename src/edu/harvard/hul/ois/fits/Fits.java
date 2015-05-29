eb4c91a530adc7a1e5408ac3a5ceeb9cee22a9b9
bbpennel <bbpennel@email.unc.edu>
2015-05-29 10:50:33 -0400

    Add file: prefix to log4j config path

--- src/edu/harvard/hul/ois/fits/Fits.java
+++ src/edu/harvard/hul/ois/fits/Fits.java
@@ -126,7 +126,7 @@ public class Fits {
     //  If log4j.debug=true is set then it shows that this doesn't actually find the specified
     //  log4j.properties file.  Leaving as is for now since overall logging works as intended.
     //  also note that any logging statements in this class probably do not work.
-    System.setProperty( "log4j.configuration", FITS_TOOLS + "log4j.properties" );
+    System.setProperty( "log4j.configuration", "file:" + FITS_TOOLS + "log4j.properties" );
 
     logger = Logger.getLogger( this.getClass() );
     try {
