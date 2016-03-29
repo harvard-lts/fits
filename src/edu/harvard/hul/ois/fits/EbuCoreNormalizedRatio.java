464fcc6d1f161dc2c411885daa030ccfd0ec5b6b
dbirtwellharvard <david_birtwell@harvard.edu>
2016-03-29 15:44:37 -0400

    Revised Aspect Ration handling for Video ebucore generation.

--- src/edu/harvard/hul/ois/fits/EbuCoreNormalizedRatio.java
+++ src/edu/harvard/hul/ois/fits/EbuCoreNormalizedRatio.java
@@ -2,27 +2,55 @@ package edu.harvard.hul.ois.fits;
 
 import java.math.BigDecimal;
 
+import org.apache.commons.lang.math.NumberUtils;
+
 /** This class handles the transformation of the numerator and denominator
- * contained within a Ratio used in Ebucore XML to nomalized values.
+ * contained within a Ratio used in Ebucore XML to normalized values.
  * For example, if the numerator is 2.22 and the denominator is 1, the 
  * numerator is converted to 222 and the denominator is converted to 100,
  * thus eliminating the decimal places, and essentially transforming them into
  * integers.
+ * 
+ * If the value passed in is null, or invalid, we will get a numerator and 
+ * denominator value of 0.
  */
 public class EbuCoreNormalizedRatio {
 
 	private int normalizedNumerator;
 	private int normalizedDenominator;
-	private final static String DECIMAL = ".";
+	public final static String DECIMAL = ".";
+	public final static String COLON = ":";	
 
-	public EbuCoreNormalizedRatio(String numerator, String denominator) {
-		//super();
+	public EbuCoreNormalizedRatio(String ratioStr) {
+		
+		// This should not happen
+		if(ratioStr == null || ratioStr.length() < 1)
+	    	return;
+		
+		// Handles the conversion of a single decimal value to an x:y ratio, if
+		// necessary
+		ratioStr = convertDecimalToRatio(ratioStr);
+		
+		// If we don't have a proper ratio, just return
+		// TODO: Should we throw an exception here?
+		if(!ratioStr.contains(COLON))
+			return;
 
-		// TODO: verify that the string passed in is a number
-		//
-		//if(!NumberUtils.isNumber(numerator)) {
-		//	
-		//}
+		String[] splitValues = ratioStr.split(EbuCoreNormalizedRatio.COLON);
+		// TODO: throw exception if there are not 2 pieces
+		if (splitValues != null && splitValues.length == 2) {
+			// Normalize the ratio, if necessary
+			normalizeRatio(splitValues[0], splitValues[1]);
+		}
+		
+	}
+	
+	private void normalizeRatio(String numerator, String denominator) {
+
+		// Verify that the strings passed in are both numbers
+		if(!NumberUtils.isNumber(numerator) || !NumberUtils.isNumber(denominator)) {
+			return;
+		}
 
 		// We only need to normalize the values if one contains a decimal
 		if(numerator.contains(DECIMAL) || denominator.contains(DECIMAL)) {
@@ -42,7 +70,24 @@ public class EbuCoreNormalizedRatio {
 		}		
 
 
-	}	
+	}
+	
+	/**
+	 * If the AspectRatio is a single decimal value, such as "1.067" this will
+	 * convert it to an x:y ratio. Otherwise it will return the original ratioStr
+	 * 
+	 * @param ratioStr The decimal representation of the aspect ratio to convert
+	 * @return
+	 */
+	private String convertDecimalToRatio(String ratioStr) {
+
+		// If this isn't a decimal value such as 1.067, we don't need to convert it
+		if (!NumberUtils.isNumber(ratioStr) || !ratioStr.contains(DECIMAL)) {
+			return ratioStr;
+		}
+		// Convert the value to an x:y representation by revising it to ratioStr:1
+		return  ratioStr + COLON + "1";
+	}
 
 	/**
 	 * Return the value required to normalize a ratio. For example, if the 
