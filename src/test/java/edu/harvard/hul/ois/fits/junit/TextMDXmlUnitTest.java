24b3a7342c2973bc477770b96e3ce59156e1d4b4
Peter Winckles <peter.winckles@wisc.edu>
2022-03-11 11:50:06 -0600

    refine output for TEI and KML

--- src/test/java/edu/harvard/hul/ois/fits/junit/TextMDXmlUnitTest.java
+++ src/test/java/edu/harvard/hul/ois/fits/junit/TextMDXmlUnitTest.java
@@ -52,74 +52,31 @@ public class TextMDXmlUnitTest extends AbstractXmlUnitTest {
 	}
     
 	@Test
-	public void testUTF16TextMD() throws Exception {	
-
-		String inputFilename = "utf16.txt";
-    	File input = new File("testfiles/" + inputFilename);
-    	FitsOutput fitsOut = fits.examine(input);
-    	fitsOut.addStandardCombinedFormat();
-    	fitsOut.saveToDisk("test-generated-output/" + inputFilename + ACTUAL_OUTPUT_FILE_SUFFIX);
-    	
-		XMLOutputter serializer = new XMLOutputter(Format.getPrettyFormat());
-		String actualXmlStr = serializer.outputString(fitsOut.getFitsXml());
-
-		// Read in the expected XML file
-		Scanner scan = new Scanner(new File(
-				"testfiles/output/" + inputFilename + EXPECTED_OUTPUT_FILE_SUFFIX));
-		String expectedXmlStr = scan.
-				useDelimiter("\\Z").next();
-		scan.close();
-
-		testActualAgainstExpected(actualXmlStr, expectedXmlStr, inputFilename);
+	public void testUTF16TextMD() throws Exception {
+		testFile("utf16.txt");
 	}
 
 	@Test
-	public void testPlainText() throws Exception {	
-		
-		String inputFilename = "plain-text.txt";
-    	File input = new File("testfiles/" + inputFilename);
-    	FitsOutput fitsOut = fits.examine(input);
-    	fitsOut.addStandardCombinedFormat();
-    	fitsOut.saveToDisk("test-generated-output/" + inputFilename + ACTUAL_OUTPUT_FILE_SUFFIX);
-    	
-		XMLOutputter serializer = new XMLOutputter(Format.getPrettyFormat());
-		String actualXmlStr = serializer.outputString(fitsOut.getFitsXml());
-
-		// Read in the expected XML file
-		Scanner scan = new Scanner(new File(
-				"testfiles/output/" + inputFilename + EXPECTED_OUTPUT_FILE_SUFFIX));
-		String expectedXmlStr = scan.
-				useDelimiter("\\Z").next();
-		scan.close();
-
-		testActualAgainstExpected(actualXmlStr, expectedXmlStr, inputFilename);
+	public void testPlainText() throws Exception {
+		testFile("plain-text.txt");
 	}
 
 	@Test
-	public void testCsv() throws Exception {	
-    	
-    	String inputFilename = "random_data.csv";
-    	File input = new File("testfiles/" + inputFilename);
-    	FitsOutput fitsOut = fits.examine(input);
-    	fitsOut.addStandardCombinedFormat();
-    	fitsOut.saveToDisk("test-generated-output/" + inputFilename + ACTUAL_OUTPUT_FILE_SUFFIX);
-    	
-		XMLOutputter serializer = new XMLOutputter(Format.getPrettyFormat());
-		String actualXmlStr = serializer.outputString(fitsOut.getFitsXml());
+	public void testCsv() throws Exception {
+		testFile("random_data.csv");
+	}
 
-		// Read in the expected XML file
-		Scanner scan = new Scanner(new File(
-				"testfiles/output/" + inputFilename + EXPECTED_OUTPUT_FILE_SUFFIX));
-		String expectedXmlStr = scan.
-				useDelimiter("\\Z").next();
-		scan.close();
-
-		testActualAgainstExpected(actualXmlStr, expectedXmlStr, inputFilename);
+	@Test
+	public void testTei() throws Exception {
+		testFile("P3EKWCX2XYWWB8Y.xml");
 	}
 
 	@Test
 	public void testKml() throws Exception {
-		String inputFilename = "KXXJXH4YTGWQ68W.kml";
+		testFile("KXXJXH4YTGWQ68W.kml");
+	}
+
+	private void testFile(String inputFilename) throws Exception {
 		File input = new File("testfiles/" + inputFilename);
 		FitsOutput fitsOut = fits.examine(input);
 		fitsOut.addStandardCombinedFormat();
