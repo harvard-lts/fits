16ea38a2003443b9483d73b714bcff32b0dc7434
David Neiman <david_neiman@harvard.edu>
2022-04-18 14:19:07 -0400

    Accommodate Droid still using log4j 1.

--- src/main/java/edu/harvard/hul/ois/fits/tools/utils/ParentLastClassLoader.java
+++ src/main/java/edu/harvard/hul/ois/fits/tools/utils/ParentLastClassLoader.java
@@ -56,6 +56,8 @@ public class ParentLastClassLoader extends ClassLoader {
 		loadByParentClassLoader.add("org.apache.xerces");
 		loadByParentClassLoader.add("org.w3c");
 		loadByParentClassLoader.add("org.jdom");
+		loadByParentClassLoader.add("org.slf4j");
+		loadByParentClassLoader.add("org.apache.log4j"); // can be removed once upgrade to Droid containing log4j2
 	}
 
 	/**
