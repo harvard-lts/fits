7fc8cdf01e17d958418aa66077ee53b0331368fb
David Neiman <david_neiman@harvard.edu>
2022-05-03 10:44:08 -0400

    Ensure updated log4j and slf4j classes get loaded.

--- src/main/java/edu/harvard/hul/ois/fits/tools/utils/ParentLastClassLoader.java
+++ src/main/java/edu/harvard/hul/ois/fits/tools/utils/ParentLastClassLoader.java
@@ -56,6 +56,8 @@ public class ParentLastClassLoader extends ClassLoader {
 		loadByParentClassLoader.add("org.apache.xerces");
 		loadByParentClassLoader.add("org.w3c");
 		loadByParentClassLoader.add("org.jdom");
+		loadByParentClassLoader.add("org.slf4j");
+		loadByParentClassLoader.add("org.apache.log4j"); // Ensure top-level reload4j classes are used instead of Droid and Tika log4j classes. Remove once upgraded to log4j2.
 	}
 
 	/**
